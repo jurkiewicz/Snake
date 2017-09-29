3f8f3d356a298269d9f20a707b0e77a0d164fa7c
jurkiewicz <jzdzisio@wp.pl>
2017-09-29 17:00:18 +0200

    Menu, score, styles, start, end, improved steering, colision with walls, eating apples, adding apples, adding new parts of snake

--- main.js
+++ main.js
@@ -5,7 +5,13 @@ document.addEventListener("DOMContentLoaded", function(){
 	var direction = 0;
 	var snake = [];
 	var s = 0;
-	var last;
+	var last = 2;
+	var score = 0;
+	var speed = 500;
+	var steer;
+	var appleX, appleY;
+	var apple;
+	var canChange = true;
 
 	onkeydown = onkeyup = function (e){
         var e = e || event;
@@ -31,9 +37,8 @@ document.addEventListener("DOMContentLoaded", function(){
 			x += 25;
 		}
     }
-    initialSnake();
 
-    function changeSnake(x, y, z){
+    function changeSnake(x, y){
     	var lastSnake = document.getElementById(last);
     	lastSnake.style.left = x + 'px';
     	lastSnake.style.top = y + 'px';
@@ -43,18 +48,59 @@ document.addEventListener("DOMContentLoaded", function(){
 		snake[0][1] = y;
 		--last;
 		if (last < 0) {
-			last = 2;
+			last = s - 1;
 		}
     }
 
-	function apple(){
-		x = 25 * Math.floor(Math.random() * 32);
-		y = 25 * Math.floor(Math.random() * 18);
-		var apple = document.createElement('div');
+	function makeApple(){
+		appleX = 25 * Math.floor(Math.random() * 32);
+		appleY = 25 * Math.floor(Math.random() * 18);
+		apple = document.createElement('div');
 		apple.className = 'apple';
-		apple.style.left = x + 'px';
-		apple.style.top = y + 'px';
+		apple.style.left = appleX + 'px';
+		apple.style.top = appleY + 'px';
 		container.appendChild(apple);
+	}
+
+	function start(){
+		var menu = document.getElementById('menu');
+		var easy = document.getElementById('easy');
+		var medium = document.getElementById('medium');
+		var hard = document.getElementById('hard');
+		var start = document.getElementById('start');
+
+		easy.addEventListener('click', function(){
+			speed = 500;
+			start.style.visibility = 'visible';
+		});
+		medium.addEventListener('click', function(){
+			speed = 300;
+			start.style.visibility = 'visible';
+		});
+		hard.addEventListener('click', function(){
+			speed = 100;
+			start.style.visibility = 'visible';
+		});
+
+		start.addEventListener('click', function(){
+			menu.style.visibility = 'hidden';
+			start.style.visibility = 'hidden';
+			initialSnake();
+			setInterval(move, speed);
+			steer = setInterval(steering, 50);
+			makeApple();
+		});		
+	}
+
+	function gameOver(){
+		var end = document.getElementById('end');
+		direction = -1;
+		clearInterval(steer);
+		end.innerHTML = 'GAME OVER!</br>Your score: ' + score + '</br>Click to play again';
+		end.style.visibility = 'visible';
+		document.addEventListener('click', function(){
+			location.reload();
+		});
 	}
 
 	function steering(){
@@ -62,66 +108,101 @@ document.addEventListener("DOMContentLoaded", function(){
 	        if(key[i]){
 	            switch (i) {
 	            	case 37: // left
-	                	if (direction != 2) {
+	                	if (direction != 2 && canChange) {
 	                		direction = 0;
+	                		canChange = false;
 	                	}	                	
 	                break;
 
 	                case 38: // up
-	                	if (direction != 3) {
+	                	if (direction != 3 && canChange) {
 	                		direction = 1;
+	                		canChange = false;
 	                	}
 	                break;
 
 	                case 39: // right
-	                    if (direction != 0) {
+	                    if (direction != 0 && canChange) {
 	                		direction = 2;
+	                		canChange = false;
 	                	}
 	                break;
 	             
 	                case 40: // down
-	                	if (direction != 1) {
+	                	if (direction != 1 && canChange) {
 	                		direction = 3;
+	                		canChange = false;
 	                	}
 	                break;
 	            }
 	        }
 	    }
 	}
-	for (var i = 0; i < snake.length; i++) {
-			last = i;
-	}
 
 	function move(){
-		console.log(last);
+		// for (var i = 3; i < snake.length; i++) {
+		// 	if ((snake[0][0] === snake[i][0] + 25 && direction === 0) ||
+		// 	(snake[0][0] === snake[i][0] - 25 && direction === 2) ||
+		// 	(snake[0][1] === snake[i][1] + 25 && direction === 1) ||
+		// 	(snake[0][1] === snake[i][1] - 25 && direction === 3)) {
+		// 		gameOver();
+		// 	}
+		// }
+		if ((snake[0][0] === 0 && direction === 0) ||
+			(snake[0][0] === 775 && direction === 2) ||
+			(snake[0][1] === 0 && direction === 1) ||
+			(snake[0][1] === 425 && direction === 3)) {
+			gameOver();
+		}
+		if (snake[0][0] === appleX && snake[0][1] === appleY) {
+			apple.remove();
+			score += 10;
+			switch(direction){
+				case 0:
+					snakePart(snake[0][0] - 25, snake[0][1]);
+	            break;
+
+	            case 1:
+	            	snakePart(snake[0][0], snake[0][1] - 25);
+	            break;
+
+	            case 2:
+	            	snakePart(snake[0][0] + 25, snake[0][1]);
+	            break;
+
+	            case 3:
+	            	snakePart(snake[0][0], snake[0][1] + 25);
+	            break;
+			}
+			makeApple();
+		}
+		canChange = true;
 		switch(direction){
 			case 0:
 				x = snake[0][0] - 25;
 				y = snake[0][1];
-				changeSnake(x, y, last);
+				changeSnake(x, y);
             break;
 
             case 1:
             	x = snake[0][0];
 				y = snake[0][1] - 25;
-				changeSnake(x, y, last);
+				changeSnake(x, y);
             break;
 
             case 2:
             	x = snake[0][0] + 25;
 				y = snake[0][1];
-				changeSnake(x, y, last);
+				changeSnake(x, y);
             break;
 
             case 3:
             	x = snake[0][0];
 				y = snake[0][1] + 25;
-				changeSnake(x, y, last);
+				changeSnake(x, y);
             break;
 		}
 	}
 
-	setInterval(move, 500);
-
-	setInterval(steering, 50);
+	start();	
 });
